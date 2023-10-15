555039f3503ca4d0a75b37b288ea7ee0e4aeef61
MADHAVENDRA-KG <madhvendra.gupta2000@gmail.com>
2023-10-15 18:55:48 +0530

    updated app.py

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ app = Flask(__name__)
 
 # Initialize model as None
 loaded_model = None
-physical_devices = tf.config.list_physical_devices('GPU') 
-tf.config.experimental.set_memory_growth(physical_devices[0], True)
+# physical_devices = tf.config.list_physical_devices('GPU') 
+# tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 def load_model():
     print('start load model')
