e4497d34c5441d6df445349dbd29835838db5cd6
MADHAVENDRA-KG <madhvendra.gupta2000@gmail.com>
2023-10-15 18:18:45 +0530

    Solution notebook and other files

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Paragraph_similarity
+## Paragraph Sentinement Similarity Projects
+1. Used a pretrained model available on tensorflow hub
+2. Write a flask application for inference
+3. Deployment done using Render
+
+
---
+++ app.py
@@ -0,0 +1,61 @@
+import json
+import pickle
+
+from flask import Flask,request,app,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import tensorflow_hub as hub
+import threading
+from numpy import dot                                           
+from numpy.linalg import norm 
+
+app = Flask(__name__)
+
+# Initialize model as None
+loaded_model = None
+physical_devices = tf.config.list_physical_devices('GPU') 
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+def load_model():
+    print('start load model')
+    global loaded_model
+    # Load the model asynchronously
+    model_url = "https://tfhub.dev/google/universal-sentence-encoder/4"  # Example model URL
+    loaded_model = hub.load(model_url)
+    print('end load model')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    
+    global loaded_model
+    
+    if loaded_model is None:
+        load_model()
+
+    def embed(input):
+        return loaded_model(input)    
+
+   
+
+    data = request.get_json()
+    print(data)
+    text1=data['text1']
+    text2=data['text2']
+    print(text1,text2)
+    message = [text1,text2]
+    print(message)
+    message_embeddings = embed(message)
+    print(message_embeddings)
+    a = tf.make_ndarray(tf.make_tensor_proto(message_embeddings))
+    cos_sim = dot(a[0], a[1])/(norm(a[0])*norm(a[1]))
+    
+
+    # Perform inference with the loaded model
+    results = cos_sim
+    print(results)
+    
+    return {"similarity score":str(results)}
+
+if __name__=="__main__":
+    app.run(debug=True)
